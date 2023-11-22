c80c326a0f70722f9c2b240aa3c8667fa8425029
falbas <farhanalbas30@gmail.com>
2023-11-22 10:10:18 +0700

    add/login route

--- app/controllers/users.controller.js
+++ app/controllers/users.controller.js
@@ -1,5 +1,6 @@
 const db = require('../configs/db.config')
-crypto = require('node:crypto');
+crypto = require('node:crypto')
+const jwt = require('jsonwebtoken')
 
 exports.register = (req, res) => {
   const { email, password, name } = req.body
@@ -24,9 +25,9 @@ exports.register = (req, res) => {
         return
       } else {
         const encpass = crypto
-          .createHmac("sha256", process.env.KEY_PASS)
+          .createHmac('sha256', process.env.KEY_PASS)
           .update(password)
-          .digest("hex");
+          .digest('hex')
 
         db.query(
           'INSERT INTO users (email, password, name, role) VALUES (?, ?, ?, ?)',
@@ -37,12 +38,55 @@ exports.register = (req, res) => {
               return
             }
 
-            res.send({
+            res.status(201).send({
               message: 'register successful',
             })
           }
         )
       }
+    }
+  )
+}
+
+exports.login = (req, res) => {
+  const { email, password } = req.body
+
+  if (!email || !password) {
+    res.status(400).send({ message: 'all input is required' })
+    return
+  }
+
+  const encpass = crypto
+    .createHmac('sha256', process.env.KEY_PASS)
+    .update(password)
+    .digest('hex')
+
+  db.query(
+    'SELECT * FROM users WHERE email = ? AND password = ?',
+    [email, encpass],
+    (err, result) => {
+      if (err) {
+        res.status(500).send({ message: err.message })
+        return
+      }
+
+      if (result.length === 0) {
+        res.status(404).send({ message: 'user does not exist' })
+        return
+      }
+
+      const token = jwt.sign(
+        { email: result[0].email, role: result[0].role },
+        process.env.TOKEN_KEY,
+        {
+          expiresIn: process.env.TOKEN_EXP,
+        }
+      )
+
+      res.send({
+        message: 'login successful',
+        token: token,
+      })
     }
   )
 }
--- app/routes/users.route.js
+++ app/routes/users.route.js
@@ -2,7 +2,8 @@ module.exports = (app) => {
   const users = require('../controllers/users.controller')
   const r = require('express').Router()
 
-  r.post('/', users.register)
+  r.post('/register', users.register)
+  r.post('/login', users.login)
 
   app.use('/api/users', r)
 }
