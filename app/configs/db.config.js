5ac157642bbc035729e7d6c66073b973858ab8f3
falbas <farhanalbas30@gmail.com>
2023-11-28 09:55:28 +0700

    update/db config

--- app/configs/db.config.js
+++ app/configs/db.config.js
@@ -1,15 +1,19 @@
 const mysql = require('mysql')
 
-const connection = mysql.createConnection({
+var pool = mysql.createPool({
+  connectionLimit: 10,
   host: process.env.DB_HOST,
+  port: 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
 })
 
-connection.connect((error) => {
-  if (error) throw error
+pool.getConnection((err) => {
+  if (err) {
+    return console.error(`error : ${err.message}`)
+  }
   console.log(`successfully connected to the ${process.env.DB_HOST}`)
 })
 
-module.exports = connection
+module.exports = pool
