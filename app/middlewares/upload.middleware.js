217f0c8d90e512ffdba1ce492c38e8085434e350
falbas <farhanalbas30@gmail.com>
2024-01-06 15:57:53 +0700

    update: upload middleware

--- app/middlewares/upload.middleware.js
+++ app/middlewares/upload.middleware.js
@@ -1,12 +1,31 @@
 const multer = require('multer')
 
-exports.upload = multer({
+const uploadFile = multer({
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
       cb(null, 'storage/')
     },
     filename: (req, file, cb) => {
+      if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+        const e = new Error('Image file not valid')
+        e.code = 'INVALID_IMAGE_FILE'
+        cb(e, false)
+      }
       cb(null, Date.now() + '-' + file.originalname)
     },
   }),
-})
+  limits: {
+    fileSize: 2000000,
+  },
+}).single('image')
+
+exports.upload = (req, res, next) => {
+  uploadFile(req, res, (err) => {
+    if (err?.code === 'LIMIT_FILE_SIZE') {
+      return res.status(400).send({ message: 'file too large' })
+    } else if (err?.code === 'INVALID_IMAGE_FILE') {
+      return res.status(400).send({ message: 'invalid image file' })
+    }
+    next()
+  })
+}
--- app/routes/posts.route.js
+++ app/routes/posts.route.js
@@ -4,10 +4,10 @@ const { auth } = require('../middlewares/auth.middleware')
 const { upload } = require('../middlewares/upload.middleware')
 
 module.exports = (app) => {
-  r.post('/', auth, upload.single('image'), posts.create)
+  r.post('/', auth, upload, posts.create)
   r.get('/', posts.readAll)
   r.get('/:id', posts.readById)
-  r.put('/:id', auth, upload.single('image'), posts.update)
+  r.put('/:id', auth, upload, posts.update)
   r.delete('/:id', auth, posts.delete)
 
   app.use('/api/posts', r)
